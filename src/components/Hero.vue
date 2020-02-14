a7b36731f45bb46fffe7e3f2def293348d18d8cb
sayching <chingsay888@gmail.com>
2020-02-14 19:12:53 +0700

    updated

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,16 @@
 <template>
-    <div>
-        <h1 class="header">hhahhahah</h1>
+    <div class="hero">
+        <div class="hero-extend">
+            <div class="hero-extend-blog">
+                <h1>Your Best Value Proposition</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consequat scelerisque elit sit amet consequat. Aliquam erat volutpat. Aliquam venenatis gravida nisl sit amet facilisis. Nullam cursus fermentum velit sed laoreet.</p>
+                <button>Start Free Trial</button>
+            </div>
+            <div class="hero-extend-blog">
+                <h1>hahahhdahf</h1>
+
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,7 +21,8 @@
 </script>
 
 <style scoped>
-    .header{
-        color: black;
+    .hero-extend-blog{
+        background: bisque;
+
     }
 </style>
